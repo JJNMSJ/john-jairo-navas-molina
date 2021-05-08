356943e11da36cf3924a4a44b39cffd54ba69415
JJNMSJ <83886060+JJNMSJ@users.noreply.github.com>
2021-05-08 17:09:33 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,54 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # # #
+        . # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . # #
+        . . # # #
+        . # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # # #
+        . # # # #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # #
+        . # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # #
+        . # # # #
+        `)
+    basic.pause(2000)
+    basic.showString("¡GRACIAS!")
+    basic.showIcon(IconNames.Heart)
+})
+basic.showString("COLEGAS PULSE EL BOTON A")
+music.playMelody("A F E F D G E F ", 125)
